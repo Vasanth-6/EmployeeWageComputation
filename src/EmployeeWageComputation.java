d2368c638abe7ab9b98eae121e868b006d52f5c5
vasu <vasanthakumarnr6@gmail.com>
2022-08-03 15:24:43 -0700

    added - employee wage per hour calculated

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -4,15 +4,22 @@ public class EmployeeWageComputation {
 
 
     public static final int FULL_TIME = 1;
+    public static final int WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
-
+        int workingHours = 0;
+        int empWage = 0;
         int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
 
-        if (empCheck == FULL_TIME)
+        if (empCheck == FULL_TIME) {
+            workingHours = 8;
             System.out.println("Employee is Present");
-        else
+        } else {
             System.out.println("Employee is Absent");
+        }
+         empWage = workingHours * WAGE_PER_HOUR;
+        System.out.println("Employee Wage = " + empWage);
     }
 }
