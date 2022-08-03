d362aaf6624f3ec5499e3ee4a7753fa77b034133
vasu <vasanthakumarnr6@gmail.com>
2022-08-03 14:46:22 -0700

    added - employee attendance

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -2,8 +2,17 @@
 
 public class EmployeeWageComputation {
 
+
+    public static final int FULL_TIME = 1;
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wage Computation");
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+
+        if (empCheck == FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
