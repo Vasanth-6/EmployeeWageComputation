5b18b74cf1beccf32ad65881d8b06eb0b5f91bad
vasu <vasanthakumarnr6@gmail.com>
2022-08-03 16:45:46 -0700

    added - calcualting wages for month

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -6,41 +6,35 @@ public class EmployeeWageComputation {
     public static final int FULL_TIME = 1;
     public static final int PART_TIME = 2;
     public static final int WAGE_PER_HOUR = 20;
+    public static final int WORKING_DAYS = 20;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
         int workingHours = 0;
         int empWage = 0;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        int totalWage = 0;
 
-      /*  if (empCheck == FULL_TIME) {
-            workingHours = 8;
-            System.out.println("Employee is Present");
+        for (int i=1; i<=20; i++) {
+            System.out.println("Day - " + i);
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            switch(empCheck) {
+                case FULL_TIME:
+                    workingHours = 8;
+                    System.out.println("Employee is Present");
+                    break;
+                case PART_TIME:
+                    workingHours = 4;
+                    System.out.println("Employee is Present");
+                    break;
+                default:
+                    workingHours = 0;
+                    System.out.println("Employee is Absent");
+            }
+            empWage = workingHours * WAGE_PER_HOUR;
+            System.out.println("Employee Wage = " + empWage);
+            totalWage = totalWage + empWage ;
         }
-        else if (empCheck == PART_TIME) {
-            workingHours = 4;
-            System.out.println("Employee is Present");
-        }
-        else {
-            System.out.println("Employee is Absent");
-        } */
-
-        switch(empCheck) {
-            case FULL_TIME:
-                workingHours = 8;
-                System.out.println("Employee is Present");
-                break;
-            case PART_TIME:
-                workingHours = 4;
-                System.out.println("Employee is Present");
-                break;
-            default:
-                workingHours = 0;
-                System.out.println("Employee is Absent");
-        }
-
-         empWage = workingHours * WAGE_PER_HOUR;
-        System.out.println("Employee Wage = " + empWage);
+        System.out.println("Total Employee Wage For a Month = " + totalWage);
     }
 }
